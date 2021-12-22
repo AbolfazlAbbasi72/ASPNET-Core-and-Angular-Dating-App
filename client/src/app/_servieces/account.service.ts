e0acc238dc11654b5fe5e4f61e821a345b1d4705
AbolfazlAbbasi72 <itmanir72@gmail.com>
2021-12-22 14:28:07 +0330

    end of section 5

--- client/src/app/_servieces/account.service.ts
+++ client/src/app/_servieces/account.service.ts
@@ -25,6 +25,17 @@ export class AccountService {
     );
   }
 
+  registerUser(model: any) {
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user: User) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
   setCurrnetUser(user: User) {
     this.currentUserSource.next(user);
   }
